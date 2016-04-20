6e88fd6ec59cd1446c727a887d2926db1b8e02f6
mnsekh111 <mnsekh111@gmail.com>
2016-04-20 01:13:54 -0400

    Added project_list.js

---
+++ js/projects.js
@@ -0,0 +1,109 @@
+project_list = [
+    {
+        name: "Collaborative Research: An Observational and Modeling Study of the Physical Processes Driving Exchanges between the Shelf and the Deep Ocean At Cape Hatteras ",
+        sponsor: "National Science Foundation ",
+        icon: "./images/project_images/nsf.jpg"
+    },
+
+    {
+        name: "Contribution of Prydz Bay Shelf Water To Antarctic Bottom Water Formation ",
+        sponsor: "National Science Foundation ",
+        icon: "./images/project_images/nsf.jpg"
+    },
+
+    {
+        name: "Collaborative Research: Connectivity in Western Atlantic Seep Populations: Oceanographic and Life History Processes Underlying Genetic Structure ",
+        sponsor: "National Science Foundation ",
+        icon: "./images/project_images/nsf.jpg"
+    },
+
+    {
+        name: "An Integrated Terrestrial-Coastal Ocean Observation and Modeling Framework for Carbon Management Decision Support ",
+        sponsor: "NASA ",
+        icon: "./images/project_images/nasa.jpg"
+    },
+
+    {
+        name: "NRA: Research Opportunities in Space and Earth Sciences - 2011 Altimeters Time Series, In Situ Observations, and Data Assimilative Numerical Modeling ",
+        sponsor: "NASA ",
+        icon: "./images/project_images/nasa.jpg"
+    },
+
+
+    {
+        name: "A Coupled Marine Enviromental Assessment and Prediction System for the Southeastern U.S. Coastal Ocean in Support of Effective Marine Ecosystem-Based Management, Efficient Marine Operations, and Resilient Coastal Communities",
+        sponsor: "NOAA",
+        icon: "./images/project_images/noaa.jpg"
+    },
+
+
+    {
+        name: "ECOHAB: Interannual Variability of PSP Toxicity in Eastern Maine: Testing the Leaky Gyre Hypothesis and Improving Regional Forecasts ",
+        sponsor: "NOAA",
+        icon: "./images/project_images/noaa.jpg"
+    },
+
+    {
+        name: "Use of a Biophysical Modeling Framework to Develop a Recruitment Index for Inclusion in Stock Assessments in the Gulf of Mexico and South Atlantic ",
+        sponsor: "NOAA",
+        icon: "./images/project_images/noaa.jpg"
+    },
+
+    {
+        name: "Estimating Discard Mortality of Shallow and Deep Water Reef Fishes Using Conventional and Telemetry Tags ",
+        sponsor: "NOAA",
+        icon: "./images/project_images/noaa.jpg"
+    },
+
+    {
+        name: "Implementation of an Operational Model for Prediction of Alexandrium fundyense Blooms in the Gulf of Maine ",
+        sponsor: "NOAA / Woods Hole Oceanographic Institution ",
+        icon: "./images/project_images/noaa.jpg"
+    },
+
+    {
+        name: "Southeast Coastal Ocean Observing Regional Association(SECOORA): An Environmental Monitoring and Assessment Program To Support Decision-Makers' Needs For Coastal and Ocean Data and Products ",
+        sponsor: "NOAA/SECOORA ",
+        icon: "./images/project_images/noaa.jpg"
+    },
+    {
+        name: "Hurricane Process Study: Numerical Model Investigation of Coastal Erosion and Accretion as a Result of Major Storm Events ",
+        sponsor: "USGS",
+        icon: "./images/project_images/USGS.jpg"
+    },
+
+    {
+        name: "Air-Sea CO2 Flux and Carbon Budget Synthesis and Modeling in the Entire Gulf of Mexico ",
+        sponsor: "USGS",
+        icon: "./images/project_images/USGS.jpg"
+    },
+
+    {
+        name: "Gulf Integrated Spill Response",
+        sponsor: "Gulf of Mexico Research Initiative",
+        icon: "./images/project_images/gulfmex.jpg"
+    },
+
+    {
+        name: "Three-dimensional Gulf Circulation and Biogeochemical Processes Unveiled by State-of-the-Art Profiling Float Technology and Data Assimilative Ocean Models ",
+        sponsor: "Gulf of Mexico Research Initiative",
+        icon: "./images/project_images/gulfmex.jpg"
+    },
+
+    {
+        name: "Development of a South Atlantic Ecosystem Model: A Coupled Marine Environmental Assessment and Prediction System for the Southeastern U.S. Coastal Ocean in Support of Effective Marine Ecosystem-Based Management ",
+        sponsor: "Department of the Interior",
+        icon: "./images/project_images/deptint.jpg"
+    },
+
+    {
+        name: "North Carolina Renewable Ocean Energy Program: Comprehensive Gulf Stream Ocean Energy Resource Assessment Using an Integrated Observing and Modeling Approach ",
+        sponsor: "UNC Coastal Studies Institute ",
+        icon: "./images/project_images/UNC_CSI_logo2.jpg"
+
+    },
+
+]
+
+
+
