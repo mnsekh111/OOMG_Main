ed62f1314ecb58db61bdb4019b91c69c90d40f20
mnsekh111 <mnsekh111@gmail.com>
2016-05-08 02:22:38 -0400

    Added current people

--- js/people.js
+++ js/people.js
@@ -1,15 +1,378 @@
 people = [
     {
+        name: "Dr. Ruoying \"Roy\" He",
+        info: "Distinguished Professor, OOMG Director <br>\
+        Department of Marine, Earth & Atmospheric Sciences<br>\
+        North Carolina State University",
+        office: "4149 Jordan Hall",
+        phone: "919-513-0249",
+        email: "rhe [at] ncsu [dot] edu",
+        website: "http://www.meas.ncsu.edu/faculty/he/he.html",
+        availability: "https://www.google.com/calendar/embed?src=rhe%40ncsu.edu&ctz=America/New_York",
+        photo:""
+
+    },
+
+    {
+        name: "Ms. Jennifer Warrillow",
+        info: "Lab Manager",
+        office: "4141 Jordan Hall",
+        phone: "919-515-0389",
+        email: "jawarril [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "Dr. Joseph Zambon",
+        info: "Research Associate",
+        office: "4140 Jordan Hall",
+        phone: "",
+        email: "",
+        website: "http://www.joezambon.com/",
+        availability: "",
+        photo:""
+
+    },
+
+
+    {
+        name: "Dr. Jeffrey Willison",
+        info: "Research Associate <br>\
+        focus: arctic modeling; coupling atmosphere-ocean-ice processes",
+        office: "4133 Jordan Hall",
+        phone: "",
+        email: "jawill20 [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "Mr. Yanlin Gong",
+        info: "Ph.D. candidate",
+        office: "4145 Jordan Hall",
+        phone: "",
+        email: "ygong3 [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "Mr. Xiangming Zeng",
+        info: "Ph.D. Candidate <br>" +
+        "focus: data assimilation; ocean modeling; machine learning",
+        office: "4133 Jordan Hall",
+        phone: "",
+        email: "",
+        website: "xzeng2 [at] ncsu [dot] edu",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "Ms. Laura McGee",
+        info: "MS student <br>" +
+        "focus: marine ecosystem and ocean dynamics modeling",
+        office: "4145 Jordan Hall",
+        phone: "",
+        email: "",
+        website: " lcmcgee [at] ncsu [dot] edu",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "Mr. Benjamin Johnson",
+        info: "Research Assistant <br>" +
+        "focus: ocean dynamics, mixing, and modeling",
+        office: " 4145 Jordan Hall",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "Dr. Wendy Woods",
+        info: "Research Associate" +
+        "focus: autonomous underwater vehicles (gliders)",
+        office: "2114 Jordan Hall",
+        phone: "",
+        email: "wlwoods2 [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "Mr. Kavit Mehta",
+        info: "IT support and application development",
+        office: "",
+        phone: "",
+        email: " kmmehta [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "Mr. Sekharan Natarajan",
+        info: "IT support and web site development",
+        office: "",
+        phone: "",
+        email: "smnatara [at] ncsu [dot] edu ",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "Ms. Caitlin Amos",
+        info: "Undergraduate Research Assistant <br>\
+        focus: marine environmental data analysis",
+        office: " 4145 Jordan Hall",
+        phone: "",
+        email: "cmamos [at] ncsu [dot] edu",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+]
+
+alumni=[
+    {
         name: "",
         info: "",
         office: "",
         phone: "",
         email: "",
         website: "",
-        availability: ""
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
 
     },
 
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
+    {
+        name: "",
+        info: "",
+        office: "",
+        phone: "",
+        email: "",
+        website: "",
+        availability: "",
+        photo:""
+
+    },
 
 
 ]
